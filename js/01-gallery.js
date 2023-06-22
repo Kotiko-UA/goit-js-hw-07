28dc27c82aa02de79e9acbc31c84d121f2ff9484
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-06-22 14:13:26 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = { galleryEl: document.querySelector('.gallery') };
+
+refs.galleryEl.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+function createMarkup(imgs) {
+  return imgs
+    .map(({ preview, original, description } = {}, idx) => {
+      return `  <li class="gallery__item">
+        <a href="${original}" class="gallery__link">
+          <img data-source="${original}" class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`;
+    })
+    .join('');
+}
+
+refs.galleryEl.addEventListener('click', onClick);
+
+function onClick(e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('gallery__image')) {
+    createModal(e);
+  }
+}
+
+function createModal(data) {
+  const instance = basicLightbox.create(
+    `
+   <img src="${data.target.dataset.source}" width="800" height="600">
+  `
+  );
+  instance.show();
+  document.addEventListener('keydown', (onClickEsc) => {});
+}
+
+function onClickEsc(e) {
+  if (e.code === 'Escape') {
+  }
+}
